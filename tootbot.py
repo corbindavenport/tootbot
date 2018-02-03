7e9e0600dacbee92687823653e21d5c41d676c35
Corbin Davenport <me@corbin.io>
2018-02-03 18:10:49 -0500

    Fix update check version

--- tootbot.py
+++ tootbot.py
@@ -259,7 +259,7 @@ try:
 	with urllib.request.urlopen("https://raw.githubusercontent.com/corbindavenport/tootbot/update-check/current-version.txt") as url:
 		s = url.read()
 		new_version = s.decode("utf-8").rstrip()
-		current_version = 3.0 # Current version of script
+		current_version = 1.0 # Current version of script
 		if (current_version < float(new_version)):
 			print('[WARN] A new version of Tootbot (' + str(new_version) + ') is available! (you have ' + str(current_version) + ')')
 			print ('[WARN] Get the latest update from here: https://github.com/corbindavenport/tootbot/releases')
